7813ec2fd17e75948ff88edeb638f2a6cb390668
Athanasios Agrafiotis <46803277+Athaagra@users.noreply.github.com>
2023-07-21 20:18:18 +0200

    Add files via upload

---
+++ Source1.cpp
@@ -0,0 +1,28 @@
+﻿//
+// Created by childcity on 20.05.19.
+//
+
+//#include "astree.hpp"
+//#include <iostream>
+#include <stdio.h>
+#include <string>
+#include "./src/EvaLLVM.h"
+
+int main(int argc, char const *argv[]) {
+	/**
+	*	Program to execute.
+	*/
+	std::string program = R"(
+		42
+	)";
+	/**
+	*	Compiler instance.
+	*/
+	EvaLLVM vm;
+
+	/**
+	* Generate LLVM IR.
+	*/
+	vm.exec(program);
+	return 0;
+}
---
+++ eva-llvm.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include "./src/EvaLLVM.h"
+
+int main(int argc, char const* argv[]) {
+	/**
+	*	Program to execute.
+	*/
+	std::string program = R"(
+		42
+	)";
+	/**
+	*	Compiler instance.
+	*/
+	EvaLLVM vm;
+
+	/**
+	* Generate LLVM IR.
+	*/
+	vm.exec(program);
+	return 0;
+}
