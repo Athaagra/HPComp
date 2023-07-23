ac76cc490f8f0ead7795fb9c9ec812d6f591479b
Athanasios Agrafiotis <46803277+Athaagra@users.noreply.github.com>
2023-07-23 15:05:12 +0200

    Update eva-llvm.cpp

--- eva-llvm.cpp
+++ eva-llvm.cpp
@@ -6,7 +6,12 @@ int main(int argc, char const* argv[]) {
 	*	Program to execute.
 	*/
 	std::string program = R"(
-		42
+		(var x 10)
+		
+		(while (> x 0)
+			(begin 
+				(set x (- x 1))
+				(printf "%d " x)))
 	)";
 	/**
 	*	Compiler instance.
